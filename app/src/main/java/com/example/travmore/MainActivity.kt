384bd33e7e601267927190d996fa0a117a68da34
b1otch <vvivek1801@gmail.com>
2022-12-21 05:59:22 +0530

    feat(Added OkHttpClient): Added the dependency

--- app/src/main/java/com/example/travmore/MainActivity.kt
+++ app/src/main/java/com/example/travmore/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.travmore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.travmore.ui.theme.TravMoreTheme
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +25,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HomePage()
                 }
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url("https://travel-places.p.rapidapi.com/")
+                    .post(null)
+                    .addHeader("X-RapidAPI-Key", "SIGN-UP-FOR-KEY")
+                    .addHeader("X-RapidAPI-Host", "travel-places.p.rapidapi.com")
+                    .build()
+
+                val response = client.newCall(request).execute()
+                Log.v("response", response.toString())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomePage() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TravMoreTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
